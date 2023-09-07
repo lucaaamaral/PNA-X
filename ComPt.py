3dba9723a3d0fecd5a446ff64bf9c08e8df87330
lucaaamaral <lucaaamaral@gmail.com>
2023-09-07 18:29:35 -0300

    feat: add graphics plot

--- ComPt.py
+++ ComPt.py
@@ -2,6 +2,7 @@ import pyvisa as visa
 import sys
 import logging
 from SharedLib import PNA
+import matplotlib.pyplot as plt
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s:%(funcName)s:%(lineno)d - %(levelname)s - %(message)s")
 logging.root.name="ComPt.py"
@@ -40,14 +41,35 @@ def parameter_config(session: visa.resources.Resource) -> None:
     print(f'Marker X?{session.query("CALCulate1:MARKer1:X?")}')
     print(f'Marker Y?{session.query("CALCulate1:MARKer1:Y?")}')
     PNA.resource_status(session)
+
+def plot_data(session: visa.resources.Resource, freq: int,
+              start_power:int, stop_power: int, 
+              meas_offset: float = 0) -> None:
     params = session.query("CALCulate1:PARameter:CATalog?")
     PNA.resource_status(session)
     session.write("CALCulate1:PARameter:SELect 'ComPt'") 
     session.write("FORMat:DATA ASCII,0")
     data = session.query("CALCulate1:DATA? FDATA")
-    print(data)
     PNA.resource_status(session)
 
+    data = data.split(",")
+    delta = (stop_power - start_power) / len(data)
+    ComPt = []
+    power = []
+    for i in range(len(data)):
+        data[i] = float(data[i]) + meas_offset
+        ComPt.append(data[0]-1)
+        power.append(start_power + delta*i)
+
+    print(data)
+
+    plt.plot(power, data, label="measured gain")
+    plt.plot(power, ComPt, label="compression target")
+    plt.xlabel(f"Power (dBm)")
+    plt.ylabel("Gain")
+    plt.title("Compression point")
+    plt.legend()
+    plt.show()
 def main() -> None:
     session = PNA.initiate_comms()
     print(f'\nConnected equipment identification: {PNA.identity(session)}\n')
@@ -59,7 +81,6 @@ def main() -> None:
     PNA.resource_status(session)
     parameter_config(session)
     PNA.resource_status(session)
-    print(f'Devices available: {session.query("CALCulate1:PARameter:CATalog?")}')
-
+    plot_data(session, 2400000000, start_power=0, stop_power=6, meas_offset=3.25)
 if __name__ == "__main__":
     main()
