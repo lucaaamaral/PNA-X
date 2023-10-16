3214fb7c74bce1e80a82859df778354c24955c70
lucaaamaral <lucaaamaral@gmail.com>
2023-10-15 22:27:05 -0300

    feat(front): introduction to starting page

--- PNA-X.py
+++ PNA-X.py
@@ -1,7 +1,15 @@
 import time
 from web_api.simpleapi import SimpleApi
 from routines.SharedLib.PNA import PNA
-
+def start_html(payload:str):
+    print(f'Recebido payload: {payload}')
+    try: 
+        with open('web-pages/index.html', 'r', encoding='utf-8') as file:
+            return SimpleApi.http_resource['html'] + file.read().encode('utf-8')
+    except:
+        print('INDEX file not found, sending 404')
+        return SimpleApi.http_header['404']
+    
 def SParamCal_html(payload:str):
     print(f'Recebido payload: {payload}')
     try: 
@@ -35,8 +43,6 @@ def visaAvailable(payload: str):
 
 if __name__ == '__main__':
     server = SimpleApi()
-    server.configure_endpoints('GET', '/teste', SParamCal_html)
-    server.configure_endpoints('POST', '/conector', checkConector)
-    server.configure_endpoints('GET', '/visaAvailable', visaAvailable)
+    server.configure_endpoints('GET', '/', start_html)
     server.begin_workers()
     
