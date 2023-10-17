d66c39ccaf625d44ee31b569f22050d1146d5d3f
lucaaamaral <lucaaamaral@gmail.com>
2023-10-17 00:39:32 -0300

    feat(api) introduction to compt logic

--- PNA-X.py
+++ PNA-X.py
@@ -41,6 +41,15 @@ def SParamCal_html(payload:str):
         print('INDEX file not found, sending 404')
         return SimpleApi.http_header['404']
     
+def ComPt_html(payload:str):
+    print(f'Recebido payload: {payload}')
+    try: 
+        with open('web-pages/ComPt.html', 'r', encoding='utf-8') as file:
+            return SimpleApi.http_resource['html'] + file.read().encode('utf-8')
+    except:
+        print('INDEX file not found, sending 404')
+        return SimpleApi.http_header['404']
+    
 def getConnectorOpt(payload:str):
     print(f'Recebido payload: {payload}')
 
@@ -112,50 +121,107 @@ def start_sparam(payload: str):
         ckit_4 = data['ckit_4']
         calibrate = data['calibrate']
         save = data['save']
+
+
+        if not (init_freq_unit in ['Hz', 'kHz', 'MHz', 'GHz'] and end_freq_unit in ['Hz', 'kHz', 'MHz', 'GHz']):
+            return SimpleApi.http_header['400']
+
     except:
         return SimpleApi.http_header['400']
 
-    if (init_freq_unit == 'MHz'): init_freq *= 1000
-    elif (init_freq_unit == 'GHz'): init_freq *= 1000000
+    
+    if (init_freq_unit == 'Hz'): pass
+    elif (init_freq_unit == 'kHz'): init_freq *= 1000
+    elif (init_freq_unit == 'MHz'): init_freq *= 1000000
+    elif (init_freq_unit == 'GHz'): init_freq *= 1000000000
     PNA.SParam.freq_start = init_freq
 
-    if (end_freq_unit == 'MHz'): end_freq *= 1000
-    elif (end_freq_unit == 'GHz'): end_freq *= 1000000
+    if (end_freq_unit == 'Hz'): pass
+    elif (end_freq_unit == 'kHz'): end_freq *= 1000
+    elif (end_freq_unit == 'MHz'): end_freq *= 1000000
+    elif (end_freq_unit == 'GHz'): end_freq *= 1000000000
     PNA.SParam.freq_stop = end_freq
 
     PNA.SParam.sweep_points = sweep_pt
     PNA.SParam.amplitude_dB = power
     PNA.SParam.average = average
 
-    # TODO: this congigure function could be simplified but testing is needed
-    PNA.SParam.configure(num=1, name='gain', meas='S21')
-    if PNA.error: return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
+    try:
+        # TODO: this congigure function could be simplified but testing is needed
+        PNA.SParam.configure(num=1, name='gain', meas='S21')
+        PNA.SParam.configure(num=2, name='InputRL', meas='S11')
+        PNA.SParam.configure(num=3, name='loss', meas='S12')
+        PNA.SParam.configure(num=4, name='OutputRL', meas='S22')
 
-    PNA.SParam.configure(num=2, name='InputRL', meas='S11')
-    if PNA.error: return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
+        if calibrate == "true":
+            # TODO: this function still requires waaaay much work to interact with the webpage - > code 100?
+            PNA.SParam.guided_calibration(connectors=[conn_1, conn_2, conn_3, conn_4], 
+                                        calkits=[ckit_1, ckit_2, ckit_3, ckit_4])
 
-    PNA.SParam.configure(num=3, name='loss', meas='S12')
-    if PNA.error: return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
-
-    PNA.SParam.configure(num=4, name='OutputRL', meas='S22')
-    if PNA.error: return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
-
-    if calibrate == "true":
-        # TODO: this function still requires waaaay much work to interact with the webpage - > code 100?
-        PNA.SParam.guided_calibration(connectors=[conn_1, conn_2, conn_3, conn_4], 
-                                    calkits=[ckit_1, ckit_2, ckit_3, ckit_4])
-        if PNA.error: return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
-
-    if save == "true":
-        PNA.SParam.save_cal_set(my_cal_set="visa_calibration"); 
-        if PNA.error: return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
-
+        if save == "true":
+            PNA.SParam.save_cal_set(my_cal_set="visa_calibration"); 
+    except Exception as e:
+        return SimpleApi.http_header['417'] + str(e).encode('utf-8')
+    
     PNA.session.close()       
 
     if PNA.error == None:
         return SimpleApi.http_header['200']
     else:
         return SimpleApi.http_header['404'] + PNA.error.encode('utf-8')
+
+def last_img (payload:str):
+    print(f'Recebido payload: {payload}')
+
+    try:
+        PNA.ComPt.last_image = 'web-pages/imagem.webp' # TODO: remove this line
+        with open(PNA.ComPt.last_image, 'rb') as file:
+            return SimpleApi.http_resource['png'] + file.read()
+    except: return SimpleApi.http_header['404']
+
+
+def start_compt(payload: str):
+    print(f'Recebido payload: {payload}')
+
+    data = json.loads(payload)
+
+    try:
+        freq =         int(float(data['freq']))
+        freq_unit =    data['freq_unit']
+        average =      int(data['average'])
+        start_pow =    int(data['start_pow'])
+        stop_pow =     int(data['stop_pow'])
+        offset =       float(data['offset'])
+
+        if not (freq_unit in ['Hz', 'kHz', 'MHz', 'GHz']):
+            return SimpleApi.http_header['400']
+    except Exception as e:
+        return SimpleApi.http_header['400']
+
+    if (freq_unit == 'Hz'): pass
+    elif (freq_unit == 'kHz'): freq *= 1000
+    elif (freq_unit == 'MHz'): freq *= 1000000
+    elif (freq_unit == 'GHz'): freq *= 1000000000
+
+    PNA.ComPt.frequency = freq
+    PNA.ComPt.average = average
+    PNA.ComPt.start_power = start_pow
+    PNA.ComPt.stop_power = stop_pow
+    PNA.ComPt.offset = offset
+    PNA.ComPt.frequency = freq
+
+    try:
+        PNA.ComPt.sweep_power()
+        PNA.ComPt.parameter_config()
+        PNA.ComPt.plot_data()
+    except Exception as e:
+        return SimpleApi.http_header['417'] + str(e).encode('utf-8')
+
+    if (PNA.error == None):
+        return SimpleApi.http_header['200']
+    else:
+        return SimpleApi.http_header['400'] + str(PNA.error).encode('utf-8')
+
 
 if __name__ == '__main__':
     server = SimpleApi()
@@ -164,6 +230,8 @@ if __name__ == '__main__':
     server.configure_endpoints('GET', '/', start_html)
     server.configure_endpoints('GET', '/style.css', style_css)
     server.configure_endpoints('GET', '/sparam', SParamCal_html)
+    server.configure_endpoints('GET', '/compt', ComPt_html)
+    server.configure_endpoints('GET', '/last_img', last_img)
 
     # VISA endpoints
     server.configure_endpoints('GET', '/visaAvailable', getVisaAvailable)
@@ -173,6 +241,7 @@ if __name__ == '__main__':
 
     # Start function
     server.configure_endpoints('POST', '/start_sparam', start_sparam)
+    server.configure_endpoints('POST', '/start_compt', start_compt)
 
 
     server.begin_workers()
